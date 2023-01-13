1030cccc1f834dd768cd6e40c2744159e1242359
Yakubov01 <lord17cl@gmail.com>
2023-01-13 16:27:11 +0500

    Part 9: Brands - Edit & Update & Confirm Delete w/ pop up using livewire

--- resources/views/livewire/admin/brand/index.blade.php
+++ resources/views/livewire/admin/brand/index.blade.php
@@ -36,10 +36,11 @@
                                     <td>{{ $brand->id }}</td>
                                     <td>{{ $brand->name }}</td>
                                     <td>{{ $brand->slug }}</td>
-                                    <td>{{ $brand->status == 1 ? 'Hidden' : 'Visible' }}</td>
+                                    <td>{{ $brand->status == '1' ? 'Hidden' : 'Visible' }}</td>
                                     <td>
-                                        <a href="#" class="btn btn-success">Edit</a>
-                                        <a href="#" class="btn btn-danger">Delete</a>
+                                        <a href="#" wire:click="editBrand({{$brand->id}})" data-bs-toggle="modal" data-bs-target="#updateBrandModal"
+                                            class="btn btn-success">Edit</a>
+                                        <a href="#" wire:click="deleteBrand({{$brand->id}})" data-bs-toggle="modal" data-bs-target="#deleteBrandModal" class="btn btn-danger">Delete</a>
                                     </td>
                                 </tr>
                             @empty
@@ -62,6 +63,8 @@
     <script>
         window.addEventListener('close-modal', event => {
             $('#addBrandModal').modal('hide');
+            $('#updateBrandModal').modal('hide');
+            $('#deleteBrandModal').modal('hide');
         })
     </script>
 @endpush
